a8e500f92e8d926449d541d18a63ba14c63fada1
antonycaporossi <antony.caporossi@gmail.com>
2020-12-19 18:21:33 +0100

    Removed MangaKita from WP Manga Reader

--- src/all/wpmangareader/src/eu/kanade/tachiyomi/extension/all/wpmangareader/WPMangaReaderFactory.kt
+++ src/all/wpmangareader/src/eu/kanade/tachiyomi/extension/all/wpmangareader/WPMangaReaderFactory.kt
@@ -6,13 +6,10 @@ import eu.kanade.tachiyomi.source.SourceFactory
 class WPMangaReaderFactory : SourceFactory {
     override fun createSources(): List<Source> = listOf(
         KomikMama(),
-        MangaKita(),
         Ngomik()
     )
 }
 
 class KomikMama : WPMangaReader("KomikMama", "https://komikmama.net", "id", "/manga-list")
-
-class MangaKita : WPMangaReader("MangaKita", "https://mangakita.net", "id", "/daftar-manga")
 
 class Ngomik : WPMangaReader("Ngomik", "https://ngomik.net", "id", "/all-komik")
